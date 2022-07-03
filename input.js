e29ee89d3a21ff08a1507d3c116012e677c184c5
ShouxTech <70824139+ShouxTech@users.noreply.github.com>
2022-07-02 20:42:58 -0400

    Update input.js

--- input.js
+++ input.js
@@ -6,6 +6,7 @@ function abc() {
     let a = 5;
     a += 2;
     callMe(a);
+    return 'hi';
 }
 
 const a = 5;
@@ -13,15 +14,20 @@ let b = 'true' != a;
 
 if ((5 == 2) && (true != false)) {
     console.log('ok');
-} else {
+} else if (!true) {
     console.log('abc');
+} else {
+    console.log('def');
 }
 
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, {a: 1, b: 2}];
 const obj = {a: 1, b: 2, c: [3, 4]};
 
+let notFive = !5
+
 const i = 0;
 while (i < 10) {
     console.log(i);
     i++;
+    break;
 }
